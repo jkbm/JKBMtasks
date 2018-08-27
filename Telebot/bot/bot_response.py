7feed26d93d44a8bb6f081afea6d421c659d4df2
jkbm <bigman_jk@hotmail.com>
2018-08-27 13:17:15 +0300

    Hotfixes

--- Telebot/bot/bot_response.py
+++ Telebot/bot/bot_response.py
@@ -86,7 +86,7 @@ def get_tasks(text, user):
                 params[list_arg[0]] = " ".join(list_arg[1:])
             task = Task.objects.create(**params)
             task.created_by_bot = bot_user
-            task.finish_date = task.start_date + timedelta(days=10)
+            task.finish_date = datetime.today + timedelta(days=10)
             task.save()
             
             answer = "Task '{0}' created. You need to complete it by {1}.".format(task.title, task.finish_date)
