4b14726591bc547a1f9d957ade64e9cd112bbca1
jkbm <bigman_jk@hotmail.com>
2018-09-03 22:31:28 +0300

    Briefing defiened

--- Telebot/misc/currency.py
+++ Telebot/misc/currency.py
@@ -8,13 +8,9 @@ ACCESS_TOKEN = "bea8698781bf4afc84bb419683a91512"
 
 url = "http://data.fixer.io/api/latest?access_key={0}".format(ACCESS_TOKEN)
 
-
-r = requests.post(url=url)
-jr = json.loads(r.text)
-
-EUR = 1
-
 def get_rates(ammount=100, curr1="USD", curr2="UAH"):
+    r = requests.post(url=url)
+    jr = json.loads(r.text)
     try:
         cbuy = jr['rates'][curr2]/jr['rates'][curr1]
         report = "{0}{1} to equals to {3}{2}".format(amount, curr1, curr2, round(float(amount)*cbuy, 2))
